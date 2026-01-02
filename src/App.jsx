037d98235897114bec459bc5c91213b839068993
M31 <foadams.dev@gmail.com>
2026-01-02 15:08:14 +0000

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import logo from './assets/NOTFLIX.svg'
-import background from './assets/radial.svg'
+import halo from './assets/halo.svg'
 import './App.css'
 import Search from "./Search"
 
@@ -16,9 +16,12 @@ export default function App(){
         <h3>Discover <span className="movies">Movies</span> That Are Worth Watching</h3>
          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
       </header>
-
+      <div className="halo">
+        <img src={halo} className="halo-image"/>
+      </div>
+      
       <main>
-        <h2>{searchTerm}</h2>
+        <p>Popular Movies</p>
       </main>
      
      
